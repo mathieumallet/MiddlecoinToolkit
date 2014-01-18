a5de7854dd7579ea3d3781c5258a784f1205ce2b
Mathieu Mallet <emhmark3@gmail.com>
2014-01-18 15:37:11 -0500

    Initial Commit

---
+++ Middlecoin Toolkit/SecondViewController.m
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.m
+//  Middlecoin Toolkit
+//
+//  Created by Mathieu Mallet on 1/18/2014.
+//  Copyright (c) 2014 Equinox Synthetics. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Middlecoin Toolkit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Middlecoin Toolkit
+//
+//  Created by Mathieu Mallet on 1/18/2014.
+//  Copyright (c) 2014 Equinox Synthetics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
